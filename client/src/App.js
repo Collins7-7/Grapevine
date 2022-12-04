7c67a0a1342ee6bf239f9e1e4315795d5380811e
COllins <collinsmuiruri77@gmail.com>
2022-12-04 14:26:55 +0300

    Created topbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
 import { useState, useEffect } from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import TopBar from "./topbar/TopBar";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [page, setPage] = useState("null");
 
   useEffect(() => {
-    fetch("/hello")
+    fetch("/posts")
       .then((r) => r.json())
-      .then((data) => setCount(data.count));
+      .then((data) => setPage(data));
   }, []);
 
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
-          <Route path="/">
-            <h1>Page Count: {count}</h1>
-          </Route>
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <TopBar/>
   );
 }
 
