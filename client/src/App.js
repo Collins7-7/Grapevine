c89c148ac4f4a47e2beb299e45e9c979bbeeaba4
COllins <collinsmuiruri77@gmail.com>
2022-12-07 00:09:08 +0300

    Added Routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,21 +6,26 @@ import Write from "./pages/write/Write";
 import Settings from "./pages/settings/Settings";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [page, setPage] = useState("null");
+  
+  const user = false;
 
-  useEffect(() => {
-    fetch("/posts")
-      .then((r) => r.json())
-      .then((data) => setPage(data));
-  }, []);
 
   return (
-    <>
-    <TopBar/>
-    <Signup/>
-    </>
+    <Router>
+     <TopBar/>
+       <Routes>
+       <Route exact path="/" element={<Home/>} />
+       <Route path="/signup" element={user? <Home/> : <Signup/>}>
+       </Route>
+       <Route path="/login" element={user? <Home/> : <Login/>}></Route>
+       <Route path="/write" element={user? <Write/> : <Signup/>}></Route>
+       <Route path="/settings" element={user? <Settings/> : <Signup/>}></Route>
+       <Route path="post/:postId" element={<Single/>}></Route>
+       </Routes>
+    </Router>
   );
 }
 
