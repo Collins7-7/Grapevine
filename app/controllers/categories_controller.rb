004f6d3508cf19b4f36a88279882e337ff79eadf
COllins <collinsmuiruri77@gmail.com>
2022-12-07 14:24:35 +0300

    Post categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,7 @@
 class CategoriesController < ApplicationController
 
+rescue_from ActiveRecord::RecordInvalid, with: :invalid_record
+
     def index
         categories = Category.all
         render json: categories
@@ -10,7 +12,20 @@ class CategoriesController < ApplicationController
         render json: category
     end
 
+    def create
+        category = Category.create!(category_params)
+        render json: category, status: :created
+    end
 
+private 
+
+ def category_params
+    params.permit(:name)
+ end
+
+ def invalid_record(invalid)
+    render json:{errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+ end
 
 
 end
