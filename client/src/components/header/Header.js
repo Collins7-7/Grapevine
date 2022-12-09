a942f96d16b5a7914e785b3f1a5c8305a90c59b0
COllins <collinsmuiruri77@gmail.com>
2022-12-10 00:40:35 +0300

    Title update

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <div className='header'>
         <div className="headerTitles">
-            <span className= "headerTitleLg">GRAPEVINE &#127815; </span>
+            <span className= "headerTitleLg">GRAPEVINE </span>
         </div>
         <img className="headerImg" src="https://images.unsplash.com/photo-1517999144091-3d9dca6d1e43?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1527&q=80" alt="" />
     </div>
