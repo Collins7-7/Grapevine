7c380540ccc0f485c8927d9d05f205280ebdd787
COllins <collinsmuiruri77@gmail.com>
2022-12-10 14:12:58 +0300

    Video

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <div className='header'>
         <div className="headerTitles">
-            <span className= "headerTitleLg">GRAPEVINE </span>
+            <span className= "headerTitleLg">GRAPEVINE</span>
         </div>
-        <img className="headerImg" src="https://images.unsplash.com/photo-1517999144091-3d9dca6d1e43?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1527&q=80" alt="" />
+        <img className="headerImg" src="https://images.unsplash.com/photo-1598237601465-af66b7475e92?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1335&q=80" alt="" />
     </div>
   )
 }
